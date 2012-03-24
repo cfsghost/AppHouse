e810ba37d43bd21972471d13b49db4ca2d0d27e9
Fred Chien <cfsghost@gmail.com>
2012-03-25 07:17:47 +0800

    Add Router class

--- apphouse.js
+++ apphouse.js
@@ -1,4 +1,3 @@
-var http = require('http');
 var config = require('config');
 var AppHouse = require('./lib/apphouse.js');
 
@@ -9,8 +8,6 @@ config.path.app = config.path.app || __dirname + '/apps'
 var appMgr = new AppHouse.AppManager(config.path.app);
 appMgr.runAll();
 
-http.createServer(function (req, res) {
-	console.log(req.header);
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('AppHouse Hosting Platform\n');
-}).listen(8000);
+/* Initializing Router */
+var router = new AppHouse.Router();
+router.run();
