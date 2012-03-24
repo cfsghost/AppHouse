4f38161eecdf278507849317214c0f008e0b4ccd
Fred Chien <cfsghost@gmail.com>
2012-03-25 06:56:02 +0800

    store connections which is created by application

--- apps/simple-server/app.js
+++ apps/simple-server/app.js
@@ -3,6 +3,6 @@ var http = require('http');
 http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.end('Simple Server\n');
-}).listen(80);
+}).listen(5000);
 
-//console.log(AppHouse);
+console.log(AppHouse);
