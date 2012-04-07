28c902d001f21826f78165ff38d31305812aa518
Fred Chien <cfsghost@gmail.com>
2012-04-08 07:46:29 +0800

    Fixed a bug that not clear runtime environment when shutdown service

--- apphouse.js
+++ apphouse.js
@@ -1,19 +1,30 @@
 var config = require('config');
 var AppHouse = require('./lib/apphouse.js');
 var path = require('path');
+var fs = require('fs');
 
 /* Config */
 config.path.app = config.path.app || path.join(__dirname, 'apps');
 config.path.log = config.path.log || path.join(__dirname, 'logs');
 config.path.runtime = config.path.runtime || path.join(__dirname, 'runtime');
 
-/* Initializing Application Manager */
-var appMgr = new AppHouse.AppManager(config.path.app, config.path.log, config.path.runtime, config.admin.tools);
-appMgr.runAll();
+/* Initializing */
+var appMgr;
+var router;
 
-/* Initializing Router */
-var router = new AppHouse.Router(appMgr);
-router.run();
+/* Initializing log directory */
+fs.mkdir(config.path.log, function() {
+	fs.mkdir(path.join(config.path.log, 'apps'), function() {
+
+		/* Initializing Application Manager */
+		appMgr = new AppHouse.AppManager(config.path.app, config.path.log, config.path.runtime, config.admin.tools);
+		appMgr.runAll();
+
+		/* Initializing Router */
+		router = new AppHouse.Router(appMgr);
+		router.run();
+	});
+});
 
 process.on('uncaughtException', function(err) {
 	console.err(err.stack);
@@ -32,8 +43,6 @@ process.on('SIGHUP', function() {
 })
 
 process.on('SIGINT', function() {
-	console.log('Stopping');
-
 	/* Stop all application */
 	appMgr.stopAll(function() {
 		process.exit(1);
