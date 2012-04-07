b4aa791191c583f36db1e579a229806991406a05
Fred Chien <cfsghost@gmail.com>
2012-04-08 04:45:10 +0800

    implement a runtime framework to manage sandbox

--- apphouse.js
+++ apphouse.js
@@ -5,9 +5,10 @@ var path = require('path');
 /* Config */
 config.path.app = config.path.app || path.join(__dirname, 'apps');
 config.path.log = config.path.log || path.join(__dirname, 'logs');
+config.path.runtime = config.path.runtime || path.join(__dirname, 'runtime');
 
 /* Initializing Application Manager */
-var appMgr = new AppHouse.AppManager(config.path.app, config.path.log, config.admin.tools);
+var appMgr = new AppHouse.AppManager(config.path.app, config.path.log, config.path.runtime, config.admin.tools);
 appMgr.runAll();
 
 /* Initializing Router */
@@ -29,3 +30,12 @@ process.on('SIGHUP', function() {
 		process.exit(1);
 	});
 })
+
+process.on('SIGINT', function() {
+	console.log('Stopping');
+
+	/* Stop all application */
+	appMgr.stopAll(function() {
+		process.exit(1);
+	});
+});
